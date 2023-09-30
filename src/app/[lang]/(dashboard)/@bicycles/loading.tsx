f5b46fb679bed65603f0ea255b990f8c51e2f561
Edvinas Momkus <emomkus@c2s7d23.42wolfsburg.de>
2023-09-30 20:44:27 +0200

    edited skeleton alignment

--- src/app/[lang]/(dashboard)/@bicycles/loading.tsx
+++ src/app/[lang]/(dashboard)/@bicycles/loading.tsx
@@ -1,12 +1,11 @@
 import { BicycleCardSkeleton } from "../components/BicycleCard"
 
 export default function Loading() {
-	const skeletons = ([...Array(3)].map((e, i) => <BicycleCardSkeleton key={i} transparency={45 - i * 5} />));
+	const skeletons = ([...Array(4)].map((e, i) => <BicycleCardSkeleton key={i} transparency={45 - i * 5} />));
 
-	const dashBoardFrameStyle = "w-full h-full items-center justify-start h-[100vh] w-[100vw] flex flex-col gap-4";
 	return (
-		<div className={dashBoardFrameStyle}>
+		<>
 			{skeletons}
-		</div>
+		</>
 	);
 }
