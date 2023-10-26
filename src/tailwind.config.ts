86a5ebd71bfbf498fe002f942ced4dc890911df9
Edvinas1122 <edvinasmomkus@gmail.com>
2023-10-26 03:59:39 +0200

    ignore it

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -18,37 +18,8 @@ const config: Config = {
     },
   },
   darkMode: "class",
-  plugins: [nextui({
-    prefix: "nextui", // prefix for themes variables
-    addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
-    defaultTheme: "light", // default theme from the themes object
-    defaultExtendTheme: "light", // default theme to extend on custom themes
-    layout: {}, // common layout tokens (applied to all themes)
-    themes: {
-      light: {
-        layout: {},
-        colors: {},
-        // layout: {
-        // disabledOpacity: "0.3", // opacity-[0.3]
-        // radius: {
-        //   small: "2px", // rounded-small
-        //   medium: "4px", // rounded-medium
-        //   large: "6px", // rounded-large
-        // },
-        // borderWidth: {
-        //   small: "1px", // border-small
-        //   medium: "1px", // border-medium
-        //   large: "2px", // border-large
-        // },}, // light theme layout tokens
-        // colors: {}, // light theme colors
-      },
-      dark: {
-        layout: {}, // dark theme layout tokens
-        colors: {}, // dark theme colors
-      },
-      // ... custom themes
-    },
-  })],
+  //@ts-ignore
+  plugins: [nextui()],
 }
 
 export default config
